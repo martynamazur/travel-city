69e0f310c45e4635f799fa8f59f9569387bd0c73
*** <127408392+martynamazur@users.noreply.github.com>
2025-06-03 19:19:33 +0200

    feat(localization): add support for multiple languages (PL, EN), + clearing code

--- lib/presentation/widget/most_bough_ticket_screen.dart
+++ lib/presentation/widget/most_bough_ticket_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:ticketapp/data/provider/ticket_db_provider.dart';
 import 'package:ticketapp/data/provider/ticket_provider.dart';
+import 'package:ticketapp/l10n/app_localizations.dart';
 import 'package:ticketapp/presentation/widget/ticket_card.dart';
 
 import '../../data/service/database/app_database.dart';
@@ -10,20 +11,22 @@ import '../../data/service/database/app_database.dart';
 class MostBoughTicket extends ConsumerWidget {
   const MostBoughTicket({super.key});
 
+
+
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final allTickets = ref.watch(getUserMostBoughTicketsProvider);
 
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Most bough ticket'),
+        title: Text(AppLocalizations.of(context)!.mostBoughTicketHeadline),
       ),
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.all(16.0),
           child: allTickets.when(
                 loading: () => const Center(child: CircularProgressIndicator()),
-                error: (error, stack) => Center(child: Text('Błąd: $error')),
+                error: (error, stack) => Center(child: Text('Error: $error')),
                 data: (fullTickets) {
                   return ListView.builder(
                     itemCount: fullTickets.length,
